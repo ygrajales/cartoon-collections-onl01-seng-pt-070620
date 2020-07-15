cbff6dac68319d64c3421aeaa9befcf21a2eb81d
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-15 03:07:51 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(roll)
+  
+  roll.each.with_index(1) {|name,i| puts "#{i}. #{name}"}
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(team)
+ 
+  elements = team.map{|planeteer| planeteer.capitalize}
+  elements.collect{|element| element + "!"}
+
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(call_list)
+  
+  call_list.any?{|call| call.length > 4}
+  
 end
 
-def find_the_cheese# code an argument here
+
+def find_the_cheese(ingredients)
+  
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  ingredients.find do |item|
+    cheese_types.include?(item)
+  end
+    
 end
